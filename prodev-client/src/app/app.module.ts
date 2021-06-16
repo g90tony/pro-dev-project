27594856a675f770ff9113672cb97169fa01a540
kadweka <husseinkatana17@gmail.com>
2021-06-17 01:03:36 +0300

    create forms for the studio profile page

--- prodev-client/src/app/app.module.ts
+++ prodev-client/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent
   ],
   imports: [
     BrowserModule
