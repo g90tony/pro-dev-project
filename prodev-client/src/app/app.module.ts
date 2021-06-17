d06e670e8c1d45c9acf41ba8fafb07e0ef06b06c
kadweka <husseinkatana17@gmail.com>
2021-06-18 00:16:28 +0300

    create component and routes for the forms

--- prodev-client/src/app/app.module.ts
+++ prodev-client/src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudiosignuppageComponent } from './studiosignuppage/studiosignuppage.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { StudioclientpageComponent } from './studioclientpage/studioclientpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    StudiosignuppageComponent,
+    StudioclientpageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: "signup",
+        component: StudiosignuppageComponent
+      },
+      {
+        path: "profile",
+        component: StudioclientpageComponent
+      },
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
