ca5ee2ebb4c6ea5eb89cba2ce59771ba57a5e0d0
Msyoka <daviemumo37@gmail.com>
2021-06-20 20:39:25 +0300

    add ts on search

--- prodev-client/src/app/search/search.component.ts
+++ prodev-client/src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  searchTerm: string;
+  @Output() searchEmiter =new EventEmitter<any>();
+
   constructor() { }
+
+  newUser() {
+    this.searchEmiter.emit(this.searchTerm);
+  }
 
   ngOnInit(): void {
   }
