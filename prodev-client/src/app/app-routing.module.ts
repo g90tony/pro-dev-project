647f34338842b70cda38943ab6bcc154eb117308
Caleb Tony <calebmbugua@gmail.com>
2021-06-24 12:53:44 +0300

    added routres for client view profile and studio edit profile

--- prodev-client/src/app/app-routing.module.ts
+++ prodev-client/src/app/app-routing.module.ts
@@ -6,13 +6,18 @@ import { StudiosignupComponent } from './studiosignup/studiosignup.component';
 import { StudiologinComponent } from './studiologin/studiologin.component';
 import { LandingComponent } from './landing/landing.component';
 import { BookingComponent } from './booking/booking.component';
+import { ClientviewprofileComponent } from './clientviewprofile/clientviewprofile.component';
+import { StudioEditProfileComponent } from './studioeditprofile/studioeditprofile.component';
 
 const routes: Routes = [
+  // client routes
   { path: 'client/signup', component: ClientsignupComponent },
   { path: 'client/login', component: ClientloginComponent },
+  { path: 'client/studio/:id', component: ClientviewprofileComponent },
+  // studio routes
   { path: 'studio/signup', component: StudiosignupComponent },
   { path: 'studio/login', component: StudiologinComponent },
-  { path: 'studio/login', component: StudiologinComponent },
+  { path: 'studio/profile/edit', component: StudioEditProfileComponent },
   { path: 'booking', component: BookingComponent },
   { path: '', component: LandingComponent },
 ];
