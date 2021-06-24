9b1855b257c9c8b3155919652fe6b4b75c0a2d84
melissa-koi <melissawangui3@gmail.com>
2021-06-24 15:29:07 +0300

    add studio user view

--- prodev-api/studioapp/urls.py
+++ prodev-api/studioapp/urls.py
@@ -20,27 +20,6 @@ urlpatterns = [
         http://127.0.0.1:8000/api/services/update/1/ === to update specific item
         http://127.0.0.1:8000/api/services/delete/1/ === to delete specific item
         
-    '''  ,
-
-    #StudioUser
-    path('api/StudioUser/', views.StudioUserList.as_view(), name='StudioUser'),
-    path('api/StudioUser/update/<int:pk>/',views.IndividualStudioUser.as_view()),
-    path('api/StudioUser/delete/<int:pk>/',views.IndividualStudioUser.as_view()),
-
-    #AdvertPost
-    path('api/advertpost/', views.AdvertPostList.as_view(), name='AdvertPost'),
-    path('api/advertpost/update/<int:pk>/',views.IndividualAdvertPost.as_view()),
-    path('api/advertpost/delete/<int:pk>/',views.IndividualAdvertPost.as_view()),
-
-    #Services
-    path('api/services/', views.ServicesList.as_view(), name='Services'),
-    path('api/services/update/<int:pk>/',views.IndividualServices.as_view()),
-    path('api/services/delete/<int:pk>/',views.IndividualServices.as_view()),
-
-    #StudioProfile
-    path('api/studioprofile/', views.StudioProfileList.as_view()),
-    path('api/studioprofile/update/<int:pk>/',views.IndividualStudioProfile.as_view()),
-    path('api/studioprofile/delete/<int:pk>/',views.IndividualStudioProfile.as_view()),
-]
+    '''
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
