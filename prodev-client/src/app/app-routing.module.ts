fbeeedc41628d1c4e6372e05f6c7046380b06b5b
Caleb Tony <calebmbugua@gmail.com>
2021-06-24 16:54:11 +0300

    added search uri

--- prodev-client/src/app/app-routing.module.ts
+++ prodev-client/src/app/app-routing.module.ts
@@ -8,18 +8,21 @@ import { LandingComponent } from './landing/landing.component';
 import { BookingComponent } from './booking/booking.component';
 import { ClientviewprofileComponent } from './clientviewprofile/clientviewprofile.component';
 import { StudioEditProfileComponent } from './studioeditprofile/studioeditprofile.component';
+import { SearchComponent } from './search/search.component';
+import { StudioclientpageComponent } from './studioclientpage/studioclientpage.component';
 
 const routes: Routes = [
+  { path: '', component: LandingComponent },
   // client routes
   { path: 'client/signup', component: ClientsignupComponent },
   { path: 'client/login', component: ClientloginComponent },
-  { path: 'client/studio/:id', component: ClientviewprofileComponent },
+  { path: 'client/view-studio/:id', component: ClientviewprofileComponent },
+  { path: 'client/search', component: SearchComponent },
   // studio routes
   { path: 'studio/signup', component: StudiosignupComponent },
   { path: 'studio/login', component: StudiologinComponent },
   { path: 'studio/profile/edit', component: StudioEditProfileComponent },
-  { path: 'booking', component: BookingComponent },
-  { path: '', component: LandingComponent },
+  { path: 'studio/profile/create', component: StudioclientpageComponent },
 ];
 
 @NgModule({
