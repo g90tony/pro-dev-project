872471fa1863d648e260cef5465e6b571dd166d9
JKimani77 <jkkimani77@gmail.com>
2021-06-24 14:12:12 +0300

    added comments and fixed indentation

--- prodev-api/studioapp/backends.py
+++ prodev-api/studioapp/backends.py
@@ -16,13 +16,7 @@ class JWTAuthentication(authentication.BaseAuthentication):
         whether the endpoint requires authentication. 
 
         `authenticate` has two possible return values:
-
-        1) `None` - We return `None` if we do not wish to authenticate. Usually
-                    this means we know authentication will fail. An example of
-                    this is when the request does not include a token in the
-                    headers.
-
-        2) `(user, token)` - We return a user/token combination when 
+         `(user, token)` - We return a user/token combination when 
                              authentication is successful.
 
                             If neither case is met, that means there's an error 
@@ -34,7 +28,7 @@ class JWTAuthentication(authentication.BaseAuthentication):
         request.user = None
 
         # `auth_header` should be an array with two elements: 1) the name of
-        # the authentication header (in this case, "Token") and 2) the JWT 
+        # the authentication header (ie, "Token") and 2) the JWT 
         # that we should authenticate against.
         auth_header = authentication.get_authorization_header(request).split()
         auth_header_prefix = self.authentication_header_prefix.lower()
@@ -48,26 +42,13 @@ class JWTAuthentication(authentication.BaseAuthentication):
             return None
 
         elif len(auth_header) > 2:
-            # Invalid token header. The Token string should not contain spaces. Do
-            # not attempt to authenticate.
             return None
-
-        # The JWT library we're using can't handle the `byte` type, which is
-        # commonly used by standard libraries in Python 3. To get around this,
-        # we simply have to decode `prefix` and `token`. This does not make for
-        # clean code, but it is a good decision because we would get an error
-        # if we didn't decode these values.
+            #handle the response which is in byte 
         prefix = auth_header[0].decode('utf-8')
         token = auth_header[1].decode('utf-8')
 
         if prefix.lower() != auth_header_prefix:
-            # The auth header prefix is not what we expected. Do not attempt to
-            # authenticate.
             return None
-
-        # By now, we are sure there is a *chance* that authentication will
-        # succeed. We delegate the actual credentials authentication to the
-        # method below.
         return self._authenticate_credentials(request, token)
 
     def _authenticate_credentials(self, request, token):
