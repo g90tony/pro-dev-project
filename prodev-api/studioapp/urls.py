ab23d878e5f17fc91505ca2184e84dab1114609c
melissa-koi <melissawangui3@gmail.com>
2021-06-24 15:37:42 +0300

    add path example to delete specific chosen item

--- prodev-api/studioapp/urls.py
+++ prodev-api/studioapp/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
        path examples:
         http://127.0.0.1:8000/api/services/ === to view all items
         http://127.0.0.1:8000/api/services/update/1/ === to update specific item
+        http://127.0.0.1:8000/api/services/delete/1/ === to delete specific item
     '''  ,
 
    #StudioUser
