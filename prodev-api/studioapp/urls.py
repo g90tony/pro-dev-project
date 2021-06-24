0a7eb88dba42a05d8af6654ee2562dcc0d6ed0a4
melissa-koi <melissawangui3@gmail.com>
2021-06-24 15:36:42 +0300

    add path example to view all given items

--- prodev-api/studioapp/urls.py
+++ prodev-api/studioapp/urls.py
@@ -13,26 +13,31 @@ urlpatterns = [
     path('api/booking/', views.CreateBooking.as_view(), name='hello'),
     path('api/review/', views.CreateReview.as_view(), name='hello'),
 
-    #StudioUser
-    path('api/StudioUser/', views.StudioUserList.as_view(), name='StudioUser'),
-    path('api/StudioUser/update/<int:pk>/',views.IndividualStudioUser.as_view()),
-    path('api/StudioUser/delete/<int:pk>/',views.IndividualStudioUser.as_view()),
+    '''
+       path examples:
+        http://127.0.0.1:8000/api/services/ === to view all items
+    '''  ,
 
-    #AdvertPost
-    path('api/advertpost/', views.AdvertPostList.as_view(), name='AdvertPost'),
-    path('api/advertpost/update/<int:pk>/',views.IndividualAdvertPost.as_view()),
-    path('api/advertpost/delete/<int:pk>/',views.IndividualAdvertPost.as_view()),
+   #StudioUser
+   path('api/StudioUser/', views.StudioUserList.as_view(), name='StudioUser'),
+   path('api/StudioUser/update/<int:pk>/',views.IndividualStudioUser.as_view()),
+   path('api/StudioUser/delete/<int:pk>/',views.IndividualStudioUser.as_view()),
 
-    #Services
-    path('api/services/', views.ServicesList.as_view(), name='Services'),
-    path('api/services/update/<int:pk>/',views.IndividualServices.as_view()),
-    path('api/services/delete/<int:pk>/',views.IndividualServices.as_view()),
+   #AdvertPost
+   path('api/advertpost/', views.AdvertPostList.as_view(), name='AdvertPost'),
+   path('api/advertpost/update/<int:pk>/',views.IndividualAdvertPost.as_view()),
+   path('api/advertpost/delete/<int:pk>/',views.IndividualAdvertPost.as_view()),
 
-    #StudioProfile
-    path('api/studioprofile/', views.StudioProfileList.as_view()),
-    path('api/studioprofile/update/<int:pk>/',views.IndividualStudioProfile.as_view()),
-    path('api/studioprofile/delete/<int:pk>/',views.IndividualStudioProfile.as_view()),
+   #Services
+   path('api/services/', views.ServicesList.as_view(), name='Services'),
+   path('api/services/update/<int:pk>/',views.IndividualServices.as_view()),
+   path('api/services/delete/<int:pk>/',views.IndividualServices.as_view()),
+
+   #StudioProfile
+   path('api/studioprofile/', views.StudioProfileList.as_view()),
+   path('api/studioprofile/update/<int:pk>/',views.IndividualStudioProfile.as_view()),
+   path('api/studioprofile/delete/<int:pk>/',views.IndividualStudioProfile.as_view()),
 
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
