b805194230411976c93fde2468688ae0d8d6369f
Caleb Tony <calebmbugua@gmail.com>
2021-06-26 17:23:14 +0300

    minor changes

--- prodev-api/studioapp/admin.py
+++ prodev-api/studioapp/admin.py
@@ -3,12 +3,8 @@ from .models import *
 from django.contrib.auth.admin import UserAdmin
 from core.settings import *
 
-
-
+# Register your models here.
 admin.site.register(User)
 admin.site.register(StudioProfile)
 admin.site.register(CreativeProfile)
 admin.site.register(Booking)
-
-
-# Register your models here.
