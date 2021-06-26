b5b3dede8632126aadda87ccce22f58d0b863115
Caleb Tony <calebmbugua@gmail.com>
2021-06-26 17:07:19 +0300

    revised url and added a get route for each studio model routes

--- prodev-api/studioapp/urls.py
+++ prodev-api/studioapp/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
    path('api/client-user/create-booking/', views.CreateBooking.as_view(), name='make_booking'),
    
    # reviews
-   path('api/client-user/add-review/', views.CreateReview.as_view(), name='hello'),
+   path('api/client-user/add-review/<int:studio_id>', views.CreateReview.as_view(), name='make_reviews'),
 
    # path examples: 
    #  http://127.0.0.1:8000/api/services/ --> to view all items
@@ -27,21 +27,25 @@ urlpatterns = [
 
    #StudioUser
    path('api/studio-user/', views.StudioUserList.as_view(), name='StudioUser'),
+   path('api/studio-user/<int:pk>/',views.IndividualStudioUser.as_view()),
    path('api/studio-user/update/<int:pk>/',views.IndividualStudioUser.as_view()),
    path('api/studio-user/delete/<int:pk>/',views.IndividualStudioUser.as_view()),
 
    #AdvertPost
    path('api/advert-post/', views.AdvertPostList.as_view(), name='AdvertPost'),
+   path('api/advert-post/<int:pk>/',views.IndividualAdvertPost.as_view()),
    path('api/advert-post/update/<int:pk>/',views.IndividualAdvertPost.as_view()),
    path('api/advert-post/delete/<int:pk>/',views.IndividualAdvertPost.as_view()),
 
    #Services
    path('api/services/', views.ServicesList.as_view(), name='Services'),
+   path('api/services/<int:pk>/',views.IndividualServices.as_view()),
    path('api/services/update/<int:pk>/',views.IndividualServices.as_view()),
    path('api/services/delete/<int:pk>/',views.IndividualServices.as_view()),
 
    #StudioProfile
    path('api/studio-user/profile/', views.StudioProfileList.as_view()),
+   path('api/studio-user/profile/<int:pk>/',views.IndividualStudioProfile.as_view()),
    path('api/studio-user/profile/update/<int:pk>/',views.IndividualStudioProfile.as_view()),
    path('api/studio-user/profile/delete/<int:pk>/',views.IndividualStudioProfile.as_view()),
 
