34bb7f896f65d3ce1d87d2c1fe58e176f7fed852
Caleb Tony <calebmbugua@gmail.com>
2021-06-28 07:03:58 +0300

    renamed module to models

---
+++ prodev-client/src/app/models/user.ts
@@ -0,0 +1,14 @@
+export class User {
+  //   constructor(id: number, username: string, email: string, user_type: number) {
+  //     this.id = id;
+  //     this.username = username;
+  //     this.email = email;
+  //     this.user_type = user_type;
+  //   }
+
+  id: number;
+  username: string;
+  email: string;
+  user_type: number;
+  token?: string;
+}
