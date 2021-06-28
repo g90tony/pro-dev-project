fd9d8f12f1fdb7597728c3ed24aff4ec2ce22732
Caleb Tony <calebmbugua@gmail.com>
2021-06-28 07:05:48 +0300

    added a wildcaerd handler
    added studiodashboard to url

--- prodev-client/src/app/app-routing.module.ts
+++ prodev-client/src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SearchComponent } from './components/search/search.component';
+
 import { ClientEditProfileComponent } from './pages/client/clienteditprofile/clienteditprofile.component';
 import { ClientloginComponent } from './pages/client/clientlogin/clientlogin.component';
 import { ClientsignupComponent } from './pages/client/clientsignup/clientsignup.component';
 import { ClientviewprofileComponent } from './pages/client/clientviewprofile/clientviewprofile.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { StudioclientpageComponent } from './pages/studio/studioclientpage/studioclientpage.component';
+import { StudiodashboardComponent } from './pages/studio/studiodashboard/studiodashboard.component';
 import { StudioEditProfileComponent } from './pages/studio/studioeditprofile/studioeditprofile.component';
 import { StudiologinComponent } from './pages/studio/studiologin/studiologin.component';
 import { StudiosignupComponent } from './pages/studio/studiosignup/studiosignup.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
+  { path: '**', redirectTo: '/' },
   // client routes
   { path: 'client/signup', component: ClientsignupComponent },
   { path: 'client/login', component: ClientloginComponent },
@@ -21,6 +23,10 @@ const routes: Routes = [
   // studio routes
   { path: 'studio/signup', component: StudiosignupComponent },
   { path: 'studio/login', component: StudiologinComponent },
+  {
+    path: 'studio/<int:user_id>/dashboard',
+    component: StudiodashboardComponent,
+  },
   { path: 'studio/profile/edit', component: StudioEditProfileComponent },
   { path: 'studio/profile/create', component: StudioclientpageComponent },
 ];
