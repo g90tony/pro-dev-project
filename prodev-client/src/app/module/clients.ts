8a763230526bdc6a44e919d159c2f46b7be38518
Caleb Tony <calebmbugua@gmail.com>
2021-06-29 13:29:42 +0300

    deleted due to new version

--- prodev-client/src/app/module/clients.ts
+++
@@ -1,7 +0,0 @@
-export class Clients {
-    constructor(
-        public avatar_url: any,
-        public name: any,
-        public bio: any,
-    ){}
-}
--- prodev-client/src/app/module/studio.ts
+++
@@ -1,8 +0,0 @@
-export class Studio {
-    constructor(
-        public avatar_url: any,
-        public name: any,
-        public bio: any,
-
-        ){}
-}
