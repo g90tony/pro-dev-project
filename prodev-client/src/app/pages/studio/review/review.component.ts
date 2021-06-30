fd5e5a74280c94fcd7300d985924978ce6e85fe0
Caleb Tony <calebmbugua@gmail.com>
2021-06-30 11:41:35 +0300

    moved to ./client

--- prodev-client/src/app/pages/studio/review/review.component.ts
+++ prodev-client/src/app/pages/studio/review/review.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Review } from '../../models/review';
+import { Review } from '../../../models/review';
 
 @Component({
   selector: 'app-review',
